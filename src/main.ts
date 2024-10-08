fd570c0f58eb79e7ed4c78e137932fd7978a7c89
unkabas <undercannabis10@gmail.com>
2024-10-08 13:41:33 -0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   dotenv.config();
+
   const app = await NestFactory.create(AppModule);
+
+  // Включение CORS с настройками
+  app.enableCors({
+    origin: 'http://localhost:3000', // Разрешаем запросы с фронтенда
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешаем указанные HTTP-методы
+    credentials: true, // Разрешаем отправку cookies, если требуется
+  });
+
   await app.listen(3001);
 }
 bootstrap();
